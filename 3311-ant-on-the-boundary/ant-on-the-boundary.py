a5a4e729cac450c2b250f1a0d1ee06fe4a5c5520
Bhaskar Reddy Vanteddu <144151346+vanteddubhaskarreddy@users.noreply.github.com>
2024-02-19 21:08:00 -0600

    Time: 43 ms (53.51%) | Memory: 16.5 MB (55.58%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.py
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.py
@@ -1,4 +1,3 @@
 class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
-        a = list(accumulate(nums)).count(0)
-        return a
+        return list(accumulate(nums)).count(0)
