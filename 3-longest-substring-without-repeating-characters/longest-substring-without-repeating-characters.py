3a32eae4a2b3ae892c78754162d298031fccbc9c
Bhaskar Reddy Vanteddu <144151346+vanteddubhaskarreddy@users.noreply.github.com>
2024-08-17 03:12:11 -0500

    Time: 95 ms (17.35%) | Memory: 16.7 MB (37.27%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,37 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0: return 0
-        d = defaultdict(int)
-        l = r = c =0
-        for r in range(len(s)):
-            d[s[r]] += 1
-            while d[s[r]] > 1:
-                d[s[l]] -= 1
-                l += 1
-            c = max(c,r-l+1)
-        return c
-
-
-
-
-
-
-
-        
-        
-        # d = defaultdict(int)
-        # i = 0
-        # c = 0
-        # j = 0
-        # while j < len(s):
-        #     # print(len(d), d, i, j)
-        #     if d[s[j]] == 1:
-        #         c = max(c, len(d))
-        #         i = i+1
-        #         j = i
-        #         d = defaultdict(int)
-        #         continue
-        #     # else:
-        #     d[s[j]] += 1
-        #     j+=1
-        # return max(c, len(d)) if len(s) > 1 else len(s)
+        d = Counter()
+        j = c = mc = 0
+        for i in range(len(s)):
+            while d[s[i]] > 0:
+                d[s[j]] -= 1
+                j+=1
+            d[s[i]]=1
+            mc = max(mc, d.total())
+        return mc
