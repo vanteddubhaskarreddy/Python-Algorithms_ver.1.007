d79e0cf3efc480c8ec6000d3739d929872be0b97
Bhaskar Reddy Vanteddu <144151346+vanteddubhaskarreddy@users.noreply.github.com>
2025-04-05 18:49:47 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.45%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -3,17 +3,13 @@ class Solution:
         stack1 = []
         stack2 = []
         for c in s:
-            print(c, stack1)
             if stack1 and c == '#':
                 stack1.pop()
             elif c != '#':
                 stack1.append(c)
         for d in t:
-            print(d, stack2)
-            print('--------')
             if stack2 and d == '#':
                 stack2.pop()
             elif d != '#':
                 stack2.append(d)
-        print(stack1, stack2)
         return True if stack1 == stack2 else False
